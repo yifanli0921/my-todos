7d88d50d04e2febe494082ae3cabb4fe4c1fba36
yifan li <yifan.li@thoguhtworks.com>
2021-11-08 01:36:46 +0800

    deploy pages

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-todolist/',
+    assetsPublicPath: './my-todos',
     proxyTable: {},
 
     // Various Dev Server settings
