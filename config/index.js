7136bbf9eddcd9ba081572f60d8aef71be1e11d2
yifan li <yifan.li@thoguhtworks.com>
2021-11-08 01:10:00 +0800

    add  assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-todolist/',
     proxyTable: {},
 
     // Various Dev Server settings
